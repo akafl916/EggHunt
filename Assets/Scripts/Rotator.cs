f4b1826c26154461dba2ba25643db07ce8b97ce0
akafl916 <awesomekidaryan@gmail.com>
2023-01-23 16:05:30 -0500

    added rotation to the capsule object (egg) in the title screen

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,15 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EggSpinner : MonoBehaviour
+public class Rotator : MonoBehaviour
 {
-    private Vector3 rotation;
+    public Vector3 rotation;
     public float speed;
 
     // Update is called once per frame
     void Update()
     {
-        rotation = new Vector3(0, 0, speed*Time.deltaTime); 
-        transform.Rotate(rotation);
+        transform.Rotate(speed*Time.deltaTime*rotation);
     }
 }
